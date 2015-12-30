a3953f03b5f13c19300d84e9a26115a413e4e70c
Luke Policinski <luke@lukepolo.com>
2015-12-30 22:58:11 +0000

    adding fee tests

--- tests/FeesTest.php
+++ tests/FeesTest.php
@@ -4,28 +4,39 @@ class FeesTest extends Orchestra\Testbench\TestCase
 {
     use \LukePOLO\LaraCart\Tests\LaraCartTestTrait;
 
-    public function testAddFee()
+    private function addFee($name, $fee = 10)
     {
         $this->laracart->addFee(
-            'testFee',
-            10,
-            false, [
-                'test' => 3,
-            ]
+            $name,
+            $fee
         );
+    }
 
-        $fee = $this->laracart->getFee('testFee');
+    public function testAddFee()
+    {
+        $this->addFee('testFeeOne');
+
+        $fee = $this->laracart->getFee('testFeeOne');
 
         $this->assertEquals('$10.00', $fee->getAmount());
     }
 
     public function testMultipleFees()
     {
+        $this->addFee('testFeeOne');
+        $this->addFee('testFeeTwo', 20);
 
+        $this->assertEquals('$10.00', $this->laracart->getFee('testFeeOne')->getAmount());
+        $this->assertEquals('$20.00', $this->laracart->getFee('testFeeTwo')->getAmount());
     }
 
     public function testRemoveFee()
     {
+        $this->addFee('testFeeOne');
+        $this->assertEquals('$10.00', $this->laracart->getFee('testFeeOne')->getAmount());
 
+        $this->laracart->removeFee('testFeeOne');
+
+        $this->assertEquals('$0.00', $this->laracart->getFee('testFeeOne')->getAmount());
     }
 }
