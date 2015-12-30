816b6940d138c356138e8b4ebcf0426c47c27ff1
Luke Policinski <luke@lukepolo.com>
2015-12-30 23:03:32 +0000

    making tests for magic functions

--- tests/MagicFunctionsTest.php
+++ tests/MagicFunctionsTest.php
@@ -6,16 +6,25 @@ class MagicFunctionsTest extends Orchestra\Testbench\TestCase
 
     public function getTest()
     {
+        $item = $this->addItem();
 
+        $this->assertEquals('option_1', $item->b_test);
     }
 
     public function setTest()
     {
+        $item = $this->addItem();
 
+        $this->test_option = 123;
+
+        $this->assertEquals(123, $item->test_option);
     }
 
     public function issetTest()
     {
+        $item = $this->addItem();
 
+        $this->assertEquals(true, isset($item->option_1));
+        $this->assertEquals(false, isset($item->testtestestestsetset));
     }
 }
