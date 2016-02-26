f86a262ecec7f1a80f485f00495dc620154b5894
Luke Policinski <luke@lukepolo.com>
2016-02-26 14:17:34 +0000

    adding relations to their set model

--- tests/Models/TestItem.php
+++ tests/Models/TestItem.php
@@ -2,12 +2,15 @@
 
 namespace LukePOLO\LaraCart\Tests\Models;
 
+use Illuminate\Database\Eloquent\Model;
+use Mockery;
+
 /**
  * Class TestItem
  *
  * @package LukePOLO\LaraCart\Tests\Models
  */
-class TestItem
+class TestItem extends Model
 {
 
     public $id = 'itemID';
@@ -22,6 +25,21 @@ class TestItem
      */
     public function findOrFail($id)
     {
-        return $this;
+        return Mockery::mock(new static);
+    }
+
+    /**
+     * Begin querying a model with eager loading.
+     *
+     * @param  array|string  $relations
+     * @return \Illuminate\Database\Eloquent\Builder|static
+     */
+    public static function with($relations)
+    {
+        if (is_string($relations)) {
+            $relations = func_get_args();
+        }
+
+        return Mockery::mock(new static);
     }
 }
