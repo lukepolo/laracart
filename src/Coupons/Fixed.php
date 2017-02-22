0cf57449ba427e4825f2825541651e4f59bcf1af
Luke Policinski <luke@lukepolo.com>
2017-02-22 15:10:03 -0500

    fixing up to looke better

--- src/Coupons/Fixed.php
+++ src/Coupons/Fixed.php
@@ -41,14 +41,15 @@ class Fixed implements CouponContract
      */
     public function discount($throwErrors = false)
     {
+        $subTotal = app(LaraCart::SERVICE)->subTotal(false);
+        $total = $subTotal - $this->value;
+
         if (config('laracart.discountOnFees', false)) {
-            $total = app(LaraCart::SERVICE)->subTotal(false) + app(LaraCart::SERVICE)->feeTotals(false) - $this->value;
-        } else {
-            $total = app(LaraCart::SERVICE)->subTotal(false) - $this->value;
+            $total = $subTotal + app(LaraCart::SERVICE)->feeTotals(false) - $this->value;
         }
 
         if ($total < 0) {
-            return app(LaraCart::SERVICE)->subTotal(false);
+            return $subTotal;
         }
 
         return $this->value;
