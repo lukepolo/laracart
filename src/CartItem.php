f95b224be59dfcaab0beebbf48a798c39a17ee5c
Joaquin Marcher <joaquin@marcher.com.uy>
2017-05-04 16:27:50 +0200

    Fixed styleci problems

--- src/CartItem.php
+++ src/CartItem.php
@@ -236,7 +236,7 @@ class CartItem
         $total = 0;
 
         foreach ($this->subItems as $subItem) {
-            $total += $subItem->price(false, $taxedItemsOnly) * ($subItem->qty?:1);
+            $total += $subItem->price(false, $taxedItemsOnly) * ($subItem->qty ?: 1);
         }
 
         if ($withTax) {
